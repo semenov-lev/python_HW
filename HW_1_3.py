07350b6ce5be07c43753824ab3615421f0234522
Lev.S <firemail.leo@gmail.com>
2021-03-18 07:59:38 +0300

    delete files from main directory

--- HW_1_3.py
+++
@@ -1,7 +0,0 @@
-for percent in range(21):
-    if percent == 1:
-        print(percent, "процент")
-    elif 1 < percent < 5:
-        print(percent, "процента")
-    elif 5 <= percent <= 20:
-        print(percent, "процентов")
